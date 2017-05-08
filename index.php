ae76d20fb574b7308ebf1b3da77b9fb8095e7f86
Jonas Byström <acidflash@Trulle.local>
2017-05-08 21:58:47 +0200

    New file

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html>
 <head>
 <title>
+Testpage
 </title>
 </head>
 <body>
 <?php
 echo "<center";
-echo "New page for all of us!"
+echo "New page for all of us!";
 echo "</center>";
 ?>
 </body>
